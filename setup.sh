855a9c7632c98a8dad0c4a1baff019ce47ef0335
Jorge <jorgetaylor99@users.noreply.github.com>
2023-11-29 20:19:55 +0000

    Put oh-my-zsh install in seperate terminal to avoid script rerun, created folders for nvim config, fixed dir path for hushlogin

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Setting up your Mac..."
+echo "Set up started."
 
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
   echo "Installing Oh My Zsh..."
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+  (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)") && echo "Oh My Zsh installed successfully."
 fi
 
 # Check for Homebrew and install if we don't have it
@@ -16,14 +16,21 @@ if test ! $(which brew); then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Update Homebrew recipes
-echo "Updating Homebrew recipes..."
+# Update Homebrew
+echo "Updating Homebrew"
 brew update
 
-# Install all our dependencies with bundle (See Brewfile)
+# Install all our dependencies with bundle
 echo "Installing dependencies from Brewfile..."
 brew tap homebrew/bundle
 brew bundle --file ./brew/Brewfile
+
+# Ensure formulae are up-to-date
+echo "Upgrading brew dependencies"
+brew upgrade
+
+# Create directory for neovim configuration if it doesn't exist
+mkdir -p ~/.config/nvim
 
 # Create symlinks
 echo "Creating symlinks..."
@@ -32,10 +39,10 @@ ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/.dotfiles/.hushlogin ~/.hushlogin
+ln -sf ~/.dotfiles/macos/.hushlogin ~/.hushlogin
 
 # Set macOS preferences - will run this last because it will reload the shell
 echo "Setting macOS preferences..."
-source ./.macos
+source ~/.dotfiles/macos/.macos
 
-echo "Mac setup is complete!"
+echo "Set up complete!"

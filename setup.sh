0b533f3e149473b3d80308d002eb53cb93240340
Jorge <jorgetaylor99@users.noreply.github.com>
2023-12-04 15:47:11 +0000

    Fixed setup script checking for software

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 
 echo "Set up started."
 
+# Check for Zsh and Git, required for Oh My Zsh and plugins installation
+if ! command -v zsh >/dev/null || ! command -v git >/dev/null; then
+  echo "Error: Zsh and Git must be installed."
+  exit 1
+fi
+
 # Install Oh My Zsh and Plugins
-if test ! $(which omz); then
+if ! command -v omz >/dev/null; then
   echo "Installing Oh My Zsh..."
   (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)") && echo "Oh My Zsh installed successfully."
   
@@ -14,32 +20,31 @@ if test ! $(which omz); then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
-
 # Install Homebrew
-if test ! $(which brew); then
+if ! command -v brew >/dev/null; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Update Homebrew
+# Update and install dependencies with Homebrew
 echo "Updating Homebrew..."
 brew update
-
-# Install all our dependencies with bundle
 echo "Installing dependencies from Brewfile..."
 brew tap homebrew/bundle
 brew bundle --file ./brew/Brewfile
-
-# Ensure formulae are up-to-date
 echo "Upgrading brew dependencies..."
 brew upgrade
 
-# Create directory for neovim configuration if it doesn't exist
-mkdir -p ~/.config/nvim
+# Ensure the .dotfiles directory exists
+if [ ! -d ~/.dotfiles ]; then
+  echo "Error: ~/.dotfiles directory not found."
+  exit 1
+fi
 
-# Create symlinks
+# Create directory for neovim configuration if it doesn't exist and create symlinks
+mkdir -p ~/.config/nvim
 echo "Creating symlinks..."
 ln -sf ~/.dotfiles/alacritty/.alacritty.yml ~/.alacritty.yml
 ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
@@ -48,7 +53,7 @@ ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/macos/.hushlogin ~/.hushlogin
 
-# Set macOS preferences - will run this last because it will reload the shell
+# Set macOS preferences
 echo "Setting macOS preferences..."
 source ~/.dotfiles/macos/.macos
 
